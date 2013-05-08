724748943aa1d6b6a9628452d5326cee169162fd
Antti Niiles <antti.niiles@gmail.com>
2013-05-08 12:07:46 +0300

    no cache

--- feelings/views.py
+++ feelings/views.py
@@ -6,6 +6,16 @@ import datetime
 from feelings import app
 from feelings import database
 
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+
 @app.route('/_clear_data')
 def clear_data():
     database.query_db('DELETE FROM feelings')
