856e7297fca0c115401d329c0adc210c15cd8e16
Antti Niiles <antti.niiles@gmail.com>
2013-08-31 13:16:02 +0300

    now draws graph using jquery flot. removed python matplotlib dependency. combined nikoniko & graph view

--- README.md
+++ README.md
@@ -3,19 +3,19 @@ feelings
 
 ![feelings](https://raw.github.com/glebb/feelings/master/feelings.png)
 
-feelings is a simple tool for gathering information about feelings of people. I use it daily (part of our daily meetings) with my distributed team. After you have used it for a while, you can identify trends and use the information e.g. in retrospectives.
+feelings is a simple tool for gathering information about feelings of people. I have used it daily (part of our daily meetings) with my distributed team. After you have used it for a while, you can identify trends and use the information e.g. in retrospectives.
 
 Live demo: http://displayofpatience.com:8080/
 
-feelings is written in Python, and it saves the data to a simple sqlite db file. It uses http://flask.pocoo.org for server side stuff.
+feelings is written in Python and Html5, and it saves the data to a simple sqlite db file. It uses http://flask.pocoo.org for server side stuff.
 
-To install it, clone the repo and first install requirements (requirements.txt). Depending on your platform, you might need to install some components manually. For example on OSX you need to install libpng (use brew). On windows you might want to install numpy and matplotlib from binary installers, if pip fails.
+To install it, clone the repo and first install python requirements (requirements.txt). 
 
-After installing the requirements you are ready to go. Execute "python tornado_server.py" to get it up and running. The app runs on port 8080 by default. Open http://your_computer_or_ip:8080/ on your browser to access it. You can also use "dev_server.py", especially if you plan to modify the source. It will run on localhost:5000, and spits out errors, which comes in handy.
+After installing the requirements you are ready to go. Execute "python tornado_server.py" to get it up and running. The app runs on port 8080 by default. Open http://your_computer_or_ip:8080/ on your browser to access it. You can also use "dev_server.py", especially if you plan to modify the source. It will run on localhost:5000 by default, and spits out errors, which comes in handy.
 
 Basics:
-  * feelings stores everything to a single table (feelings.sqlite). You can access it by any means, and process the data as you please. 
-  * feelings web app provides following views: the main view (/) asks how you are doing today. By submitting the form the answer gets saved to the db. /show_avg displays averages by day including comments. /show_graph displays a graph of averages and /nikoniko shows a calendar view with individual answers color-coded day by day (excluding the last day, to provide sense of security for providing sensitive data)
+  * feelings stores everything to a single-file db (feelings.sqlite). You can access it by any means, and process the data as you please. 
+  * feelings web app provides following views: the main view (/) asks how you are doing today. By submitting the form the answer gets saved to the db. /show_avg displays averages by day including comments. /results displays a graph of averages with a "nikoniko" calendar view with individual answers color-coded day by day 
   * If you want to use feelings with multiple teams, you can define a category using a GET paramerter. Just append ?category=something at the end of the urls. E.g. http://localhost:8080/?category=team1 or http://localhost:8080/show_avg?category=team1 
   * TIP: you can use Excel to import data (set feelings /show_avg url as web data source and select the table with data) and to draw (nicer) graphs.
   
--- feelings/views.py
+++ feelings/views.py
@@ -5,11 +5,10 @@ from flask import send_from_directory
 from flask import make_response
 import os
 import datetime
+import calendar
 
 from feelings import app
 from feelings import database
-
-from feelings.graphs import create_graph
 
 DEFAULT_CATEGORY = 'test'
 ALL = '_all_'
@@ -23,6 +22,11 @@ def add_header(response):
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
     response.headers['Cache-Control'] = 'public, max-age=0'
     return response
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+            'favicon.ico', mimetype = 'image/vnd.microsoft.icon')        
 
 @app.route('/_clear_data')
 def clear_data():
@@ -42,42 +46,21 @@ def show_data():
     resp = database.query_db(query, args)
     return render_template('data.html', data = resp)
 
-@app.route('/json_data')
-def json_data():
-    cat = request.args.get('category')
-    args = []
-
-    query = "select date, group_concat(feeling) as feelings from feelings"
-    if cat:
-        query = query + " WHERE category LIKE ?"
-        args.append(cat)
-    
-    query = query + " group by date order by date ASC"
-    resp = database.query_db(query, args)
-    for row in resp:
-        row['feelings'] = row['feelings'].split(',')
-        row['feelings'].sort() 
-    resp.pop()
-    return jsonify(data = resp[-20:])
-
-    
-@app.route('/nikoniko')
-def nikoniko():
-    return render_template('nikoniko.html', category = request.args.get('category'))
+@app.route('/')
+def index():
+    category = request.args.get('category')
+    if not category:
+        category = DEFAULT_CATEGORY
+    return render_template('index.html', cat = category, today = datetime.date.today().strftime('%d.%m.%Y'))
 
 @app.route('/show_avg')
 def show_avg():
     resp = database.get_averages(request.args.get('category'))
     return render_template('data_avg.html', data = resp)
 
-@app.route('/show_graph')
-def show_graph():
-    category = request.args.get('category')
-    resp = database.get_averages(category)
-    if not category:
-        category = ALL
-    return render_template('data_graph.html', data = resp, category = category)
-
+@app.route('/results')
+def results():
+    return render_template('results.html', category = request.args.get('category'))
 
 @app.route('/thanks', methods = ['POST'])
 def add_entry():
@@ -91,29 +74,40 @@ def add_entry():
     database.save_db()
     return render_template('thanks.html', category = request.form['category'])
 
-@app.route('/')
-def index():
+
+@app.route('/json_nikoniko_data')
+def json_data():
+    cat = request.args.get('category')
+    args = []
+
+    query = "select date, group_concat(feeling) as feelings from feelings"
+    if cat:
+        query = query + " WHERE category LIKE ?"
+        args.append(cat)
+    
+    query = query + " group by date order by date ASC"
+    resp = database.query_db(query, args)
+    for row in resp:
+        temp = []
+        date_from_data = datetime.datetime.strptime(row['date'], '%Y-%m-%d')
+        temp.append(calendar.timegm(date_from_data.timetuple()) * 1000)
+        row['feelings'] = row['feelings'].split(',')
+        row['feelings'].sort() 
+    return jsonify(data = resp[-20:])
+
+@app.route('/json_graph_data')
+def create_javascript_graph():
     category = request.args.get('category')
-    if not category:
-        category = DEFAULT_CATEGORY
-    return render_template('index.html', cat = category, today = datetime.date.today().strftime('%d.%m.%Y'))
+    resp = database.get_averages(category)
 
-@app.route("/<category>/graph.png")
-def dynamic_graph(category):
-    cat = category
-    if cat == ALL:
-        cat = None
-    resp = database.get_averages(cat)
-    png = create_graph(resp, cat)
-    if png:
-        response = make_response(png)
-        response.headers['Content-Type'] = 'image/png'
-        return response
-    else:
-        return "No data for: " + cat
+    results = []
+    for row in resp:
+        temp = []
+        temp.append(_convert_to_javascript_timestamp(row['date']))
+        temp.append(row['feelingavg'])
+        results.append(temp)
+    return jsonify(data = results[-20:])
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-            'favicon.ico', mimetype = 'image/vnd.microsoft.icon')
-        
+def _convert_to_javascript_timestamp(date):
+    date_from_data = datetime.datetime.strptime(date, '%Y-%m-%d')
+    return calendar.timegm(date_from_data.timetuple()) * 1000
